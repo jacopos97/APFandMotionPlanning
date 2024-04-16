d59e7135d816d5e5b1a65e3f6fe264f99a7b3988
jacopo <jacopo.sabatino@edu.unifi.it>
2024-04-16 16:13:36 +0200

    Added repulsive potential.

--- APFandMotionPlanning.py
+++ APFandMotionPlanning.py
@@ -7,7 +7,8 @@ import networkx as nx
 import random
 import math
 import numpy as np
-import threading
+import sympy as sp
+from matplotlib.patches import PathPatch
 
 
 def random_point(r1, r2, obs, name):
@@ -61,19 +62,15 @@ def node_color(g_nodes, path):
     return c
 
 
-def generate_obstacles(obs_number, obs, box_range):
+def generate_obstacles(obs_number, obs, box_range, color):#TODO: modifica la funzione in modo che eviti di generare ostacoli sul robot(utilizza la security distance)
     while len(obs) < obs_number:
         a = random.randrange(0, box_range)
         b = random.randrange(0, box_range)
-        ob = patches.Rectangle((a, b), random.randint(1, 15), random.randint(1, 15), color='silver')
+        ob = patches.Rectangle((a, b), random.randint(1, 15), random.randint(1, 15), color=color)
         if len(obs) == 0:
             obs.append(ob)
-            #boxes.append(obs.get_bbox())
-            #ax.add_patch(obs)
         elif len(obs) > 0 and not obs_collision(obs, ob):
             obs.append(ob)
-            #boxes.append(obs.get_bbox())
-            #ax.add_patch(obs)
         else:
             pass
     return obs
@@ -129,60 +126,131 @@ def new_node_and_edges(obs, graph, graph_nodes, node_number, parameter, r1, r2):
             graph.add_edge(str(node_number), name)
 
 
-def apf(ax, nodes, path, margin, step_size, fig, attractive_delta):
-    point, = ax.plot(nodes["start"][0], nodes["start"][1], 'o', color='green')
-    point_pos = get_point_coords(point)
+def get_robot_obstacles(robot_pos, obstacles, sensor_range):
+    near_obs = []
+    for ob in obstacles:
+        bbox = ob.get_bbox()
+        distance_x = max(bbox.x0 - robot_pos[0], 0, robot_pos[0] - bbox.x1)
+        distance_y = max(bbox.y0 - robot_pos[1], 0, robot_pos[1] - bbox.y1)
+        if np.sqrt(distance_x ** 2 + distance_y ** 2) <= sensor_range:
+            x_diff = 0
+            y_diff = 0
+            if distance_x == 0:
+                x_diff = 0
+            elif bbox.x0 - robot_pos[0]:
+                x_diff = robot_pos[0] - bbox.x0
+            else:
+                x_diff = robot_pos[0] - bbox.x1
+            if distance_y == 0:
+                y_diff = 0
+            elif bbox.y0 - robot_pos[1]:
+                y_diff = robot_pos[1] - bbox.y0
+            else:
+                y_diff = robot_pos[1] - bbox.y1
+            near_obs.append((x_diff, y_diff))
+    return near_obs
+
+
+def apf(ax, nodes, path, margin, step_size, fig, attractive_delta, sensor_range, attr_k, rep_k, sec_dist, obstacles, repulsive_potential_beta):
+    point, = ax.plot(nodes["start"][0], nodes["start"][1], 'o', color='dodgerblue')
+    circle = plt.Circle(nodes["start"], sensor_range, color='lightblue', fill=True, zorder=0)
+    ax.add_artist(circle)
+    ax.set_aspect('equal', adjustable='box')
+    point_pos = get_point_coordinates(point)
     positions = [point_pos]
+    x, y = sp.symbols('x y')
+    rep_coordinates = (x, y)
+    rep_gradient_func = [sp.diff(repulsive_potential(rep_coordinates, sensor_range, sec_dist, repulsive_potential_beta), coord) for coord in rep_coordinates]
+    #print(path)
     for i in range(1, len(path)):
         sub_goal = nodes[path[i]]
-        while not loop_condition(sub_goal, point_pos, margin, attractive_delta):
-            point_pos = move_to_goal(sub_goal, point_pos, step_size, attractive_delta)
+        #print(path[i])
+        #print(sub_goal)
+        #print(point_pos)
+        #while not loop_condition(sub_goal, point_pos, margin, attractive_delta):
+        while not loop_condition(sub_goal, point_pos, margin):
+            near_obs = get_robot_obstacles(point_pos, obstacles, sensor_range)
+            point_pos = move_to_goal(sub_goal, point_pos, step_size, attractive_delta, attr_k, rep_k, near_obs, rep_gradient_func, rep_coordinates)
             positions.append(point_pos)
-    ani = FuncAnimation(fig, update, frames=len(positions), fargs=(point, positions), interval=0.5, repeat=False)
+    ani = FuncAnimation(fig, update, frames=len(positions), fargs=(point, circle, positions), interval=1, repeat=False)
     return ani
 
 
-def update(frame, point, positions):
-    x = [positions[frame][0]]
-    y = [positions[frame][1]]
-    point.set_data(x, y)
-    return point,
+def update(frame, point, circle, positions):
+    x = positions[frame][0]
+    y = positions[frame][1]
+    point.set_data([x], [y])
+    circle.center = (x, y)
+    return point, circle
 
 
-def move_to_goal(goal, point_pos, step_size, attractive_delta):
-    anti_grad = anti_gradient(goal, point_pos, attractive_delta)
+def move_to_goal(goal, point_pos, step_size, attractive_delta, attr_k, rep_k, near_obs, repulsive_gradient_func, repulsive_coordinates):
+    attr_anti_grad = attractive_anti_gradient(goal, point_pos, attractive_delta)
+    rep_anti_grad = repulsive_anti_gradient(repulsive_gradient_func, repulsive_coordinates, near_obs)
+    anti_grad = attr_k*attr_anti_grad + rep_k*rep_anti_grad
+    '''if not np.all(rep_anti_grad == 0):
+        print("")
+        print(attr_anti_grad)
+        print(rep_anti_grad)
+        print(anti_grad)'''
     point_pos = point_pos + step_size * anti_grad
-    return point_pos
+    return point_pos[0], point_pos[1]
 
 
-def anti_gradient(goal, point_pos, attractive_delta):
-    difference = np.array(goal) - np.array(point_pos)
+def attractive_anti_gradient(goal, point_pos, attractive_delta):
+    difference = np.array(goal, dtype=np.float32) - np.array(point_pos, dtype=np.float32)
     if np.linalg.norm(difference) > attractive_delta:
         return difference/np.linalg.norm(difference)
     else:
         return difference
 
 
-def loop_condition(goal, point_pos, margin, attractive_delta):
-    return np.all(np.abs(anti_gradient(goal, point_pos, attractive_delta)) <= margin)
+def repulsive_anti_gradient(gradient_func, coordinates, near_obs):
+    gradient = [0, 0]
+    for ob_pos in near_obs:
+        point = {coordinates[0]: ob_pos[0], coordinates[1]: ob_pos[1]}
+        for i in range(len(gradient_func)):
+            gradient[i] += gradient_func[i].subs(point)
+    anti_gradient = np.array([-1 * el for el in gradient], dtype=np.float32)
+    #print(anti_gradient)
+    return anti_gradient
 
 
-def get_point_coords(point):
-    coords = (point.get_xdata()[0], point.get_ydata()[0])
-    return coords
+def repulsive_potential(coordinates, sensor_range, sec_dist, repulsive_potential_beta):
+    first_operand = 1/(sp.sqrt(coordinates[0]**2+coordinates[1]**2)-sec_dist)
+    second_operand = 1/(sensor_range-sec_dist)
+    return (1/repulsive_potential_beta)*((first_operand-second_operand) ** repulsive_potential_beta)
+
+
+def loop_condition(goal, point_pos, margin):
+    distance = np.array(goal) - np.array(point_pos)
+    #print(distance)
+    return np.linalg.norm(distance) <= margin
+
+
+def get_point_coordinates(point):
+    coordinates = (point.get_xdata()[0], point.get_ydata()[0])
+    return coordinates
 
 
 def main():
-    obs_number = 10
+    known_obs_number = 10
+    unknown_obs_number = 5
     eps = 6
     num_iter = 1000
-    margin = 0.001
-    step_size = 0.15
+    margin = 0.75
+    step_size = 0.1
     attractive_delta = 0.3
+    sensor_range = 5
+    attractive_constant = 1
+    repulsive_constant = 1
+    security_distance = 0.3
+    repulsive_potential_beta = 2
 
     fig, ax = plt.subplots(figsize=(15, 15))
     obs = []
-    obs = generate_obstacles(obs_number, obs, 100)
+    obs = generate_obstacles(known_obs_number, obs, 100, 'silver')
+    #print(obs)
     plot_obs(ax, obs)
 
     start = random_point(2, 20, obs, 'start')
@@ -194,9 +262,10 @@ def main():
 
     plt.show(block=False)
 
-    plt.pause(0.4)
+    plt.pause(0.5)
     ax.cla()
-    plot_obs(ax, obs)
+    #obs += generate_obstacles(known_obs_number+unknown_obs_number, obs, 100, 'red')
+    plot_obs(ax, obs)#TODO: fai apparire i nuovi ostacoli prima di far partire l'animazione
     shortest_path = nx.shortest_path(graph, 'start', 'goal')
     path_colors = []
     for node in shortest_path:
@@ -208,9 +277,9 @@ def main():
     plt.text(goal[0], goal[1], 'goal', fontsize=12)
     nx.draw_networkx_nodes(graph, pos=nodes_g, node_color=path_colors, nodelist=shortest_path, node_size=30)
     nx.draw_networkx_edges(graph, pos=nodes_g, edgelist=list(nx.utils.pairwise(shortest_path)))
-    ani = apf(ax, nodes_g, shortest_path, margin, step_size, fig, attractive_delta)
+    ani = apf(ax, nodes_g, shortest_path, margin, step_size, fig, attractive_delta, sensor_range, attractive_constant, repulsive_constant, security_distance, obs, repulsive_potential_beta)
     plt.draw()
-    plt.show()
+    plt.show()#TODO: risolvi i possibili minimi locali
 
 
 if __name__ == '__main__':
